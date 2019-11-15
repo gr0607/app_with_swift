67abcf3029ef9819dfae63da4b1ef80ade2bb037
Alex <work.off.on@gmail.com>
2019-11-15 21:50:34 +0300

    add simple project with TabBar Controller

---
+++ AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Admin on 15/11/2019.
+//  Copyright © 2019 AlexInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
